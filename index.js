ded3cd2b83a9dd60dce3d2d8a8c6802f91d78788
Quince Lewis <106302729+quinceosanya@users.noreply.github.com>
2022-06-21 10:14:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your code in this file!
+const currentUser = 'Quince Lewis';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}`;
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const welcomeMessage = currentUser;
+const welcomeMessage = 'Welcome to EliteBook, currentUser';
+AssertionError: expected 'Welcome to EliteBook, currentUser' to contain 'Quince Lewis'
+typeof "const currentUser = 'Quince Lewis';";
+//=> "string"
+
+"const currentUser = 'Quince Lewis';".length;
+//=> 35
+
+currentUser;
+//=> Uncaught ReferenceError: currentUser is not define
+
+const currentUser = 'Quince Lewis';
+
+const welcomeMessage = 'Welcome to EliteBook, ' + currentUser;
+...
+
+const shortGreeting = 'Welcome'${currentUser}`;
+shortGreeting
+  contains "Welcome, "
+    AssertionError: expected '' to contain 'Welcome, '
